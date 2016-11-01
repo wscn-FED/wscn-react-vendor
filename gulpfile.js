2fd5e9363a57f6ee3102a37d308ff24921469134
Pan <panfree23@gmail.com>
2016-11-01 14:51:21 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,12 @@ var nodeModulesDir = path.join(__dirname, './node_modules');
 
 
 var reactProdPath = path.join(nodeModulesDir, 'react/dist/react-with-addons.min.js');
+var reactDevPath = path.join(nodeModulesDir, 'react/dist/react-with-addons.js');
 var reactDomPath = path.join(nodeModulesDir, 'react-dom/dist/react-dom.min.js');
 var axiosPath = path.join(nodeModulesDir, 'axios/dist/axios.min.js');
 var coreJs=path.join('./', 'core-js.min.js');
 
-gulp.task('default',['uglify'],function(){});
+gulp.task('default',['uglify','dev'],function(){});
 
 gulp.task('uglify',function(){
     var commons = [
@@ -25,9 +26,20 @@ gulp.task('uglify',function(){
         axiosPath,
         coreJs
     ];
-
     return gulp.src(commons)
         .pipe(concat('wscn-react-vendor.min.js'))
         .pipe(uglify(uglifyOpt))
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('dev',function(){
+    var commons = [
+        reactDevPath,
+        reactDomPath,
+        axiosPath,
+        coreJs
+    ];
+    return gulp.src(commons)
+        .pipe(concat('wscn-react-vendor.js'))
         .pipe(gulp.dest('dist/'))
 })
